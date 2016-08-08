9b87e6e2cd42c20967d5c34ec9b007421a37bd03
omkarmore83 <omkar.more@gmail.com>
2016-08-07 23:17:34 -0700

    Added code for Steps problem and concentric rectangles

---
+++ ProgrammingPuzzles/Volume1/Volume1/PrintConcentricRectangles/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintConcentricRectangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 6;
+            var result = new Program().prettyPrint(size);
+            for (int i = 0; i < size * 2 - 1; i++)
+            {
+                for (int j = 0; j < size * 2 - 1; j++)
+                    Console.Write(result[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        public int[,] prettyPrint(int a)
+        {
+            int size = (2 * a) - 1;
+            int[,] resultArr = new int[size, size];
+            for (int i = a; i >= 1; i--)
+            {
+                fillArr(resultArr, i, i, size);
+            }
+            return resultArr;
+        }
+
+        public void fillArr(int[,] resultArr, int pos, int val, int size)
+        {
+            int offset = (size + 1) / 2 - val;
+            for (int i = offset; i < size - offset; i++)
+                resultArr[offset, i] = val;
+
+            for (int i = offset; i < size - offset; i++)
+                resultArr[size - offset - 1, i] = val;
+
+            for (int i = offset; i < size - offset; i++)
+                resultArr[i, offset] = val;
+
+            for (int i = offset; i < size - offset; i++)
+                resultArr[i, size - offset - 1] = val;
+
+
+        }
+    }
+}
---
+++ ProgrammingPuzzles/Volume1/Volume1/StepsProblem/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepsProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            int n = 4; // total number of steps on the ladder
+            int m = 3; // max steps taken by the child
+
+            List<List<int>> resultSteps = GetStepsCombinations(n, m);
+
+            foreach (var combination in resultSteps)
+            {
+                foreach (var step in combination)
+                {
+                    Console.Write(step);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static List<List<int>> GetStepsCombinations(int n, int m)
+        {
+            List<List<int>> resultSteps = new List<List<int>>();
+
+            for (int i = 1; i <= m; i++)
+            {
+                if ((n - i) == 0)
+                {
+                    var singleCombination = new List<int>();
+                    singleCombination.Add(i);
+                    resultSteps.Add(singleCombination);
+                    return resultSteps;
+
+                }
+                var subResultSteps = GetStepsCombinations(n - i, m);
+                foreach (var combination in subResultSteps)
+                {
+                    combination.Add(i);
+                    resultSteps.Add(combination);
+                }
+            }
+            return resultSteps;
+        }
+    }
+}
